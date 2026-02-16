9db7df323767559ccc28848edb03e9a3d755111f
RashmitTopG <rashmit449@gmail.com>
2026-02-16 10:43:24 +0530

    feat: Added input types and loader

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,22 @@
 import { forwardRef, type ChangeEvent } from "react";
 
+
+
 interface InputProps {
   value?: string;
   placeholder: string;
+  type? : "text" | "password";
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ value, placeholder, onChange }, ref) => {
+  ({ value, placeholder, onChange , type}, ref) => {
     return (
       <input
         ref={ref}
         value={value}
         placeholder={placeholder}
-        type="text"
+        type={type}
         className="px-4 py-2 border rounded m-2"
         onChange={onChange}
       />
